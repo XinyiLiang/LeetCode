29daae7c5aff10cdf5ab2a90d351a277fc573f8f
Xinyi Liang <38815750+XinyiLiang@users.noreply.github.com>
2020-12-04 12:53:50 -0500

    Create The kth Factor of n.java

---
+++ java/The kth Factor of n.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int kthFactor(int n, int k) {
+        ArrayList<Integer> list = new  ArrayList<Integer>();
+        
+        for ( int i =1; i<= n; i++)
+        {
+           if (n%i==0)
+               list.add(i);
+        }
+        
+        if (k<= list.size())
+        {
+            return list.get(k-1);
+        }
+        
+        return -1;
+        
+    }
+}
